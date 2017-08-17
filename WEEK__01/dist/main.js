9ea90c845bb27a7c69d88366ea418e8b5d920845
Jee Hoon <yamoo9@naver.com>
2017-08-17 14:03:41 +0900

    src/main.js로 코드 이동

--- WEEK__01/dist/main.js
+++ WEEK__01/dist/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 /*! main.js @ 2017, yamoo9.net */
+
+// ——————————————————————————————————————
+// JavaScript 약점
+// 동적 형 지정 언어
+// ——————————————————————————————————————
 
 // ——————————————————————————————————————
 // JavaScript 약점
@@ -19,23 +26,25 @@ num = 90;
 str = 'tranning';
 boo = false;
 // fnc = function(){}; // new Function()
-fnc = () => {}; // ES 6 Arrow Function
-arr = [];           // new Array()
-obj = {};           // new Object
+fnc = function fnc() {}; // ES 6 Arrow Function
+arr = []; // new Array()
+obj = {}; // new Object
 
-console.log('typeof num:', typeof num); // 'number'
-console.log('typeof str:', typeof str); // 'string'
-console.log('typeof boo:', typeof boo); // 'boolean'
-console.log('typeof fnc:', typeof fnc); // 'function'
-console.log('typeof arr:', typeof arr); // 'array'
-console.log('typeof obj:', typeof obj); // 'object'
+console.log('typeof num:', typeof num === 'undefined' ? 'undefined' : _typeof(num)); // 'number'
+console.log('typeof str:', typeof str === 'undefined' ? 'undefined' : _typeof(str)); // 'string'
+console.log('typeof boo:', typeof boo === 'undefined' ? 'undefined' : _typeof(boo)); // 'boolean'
+console.log('typeof fnc:', typeof fnc === 'undefined' ? 'undefined' : _typeof(fnc)); // 'function'
+console.log('typeof arr:', typeof arr === 'undefined' ? 'undefined' : _typeof(arr)); // 'array'
+console.log('typeof obj:', typeof obj === 'undefined' ? 'undefined' : _typeof(obj)); // 'object'
 
 // 네임스페이스 객체 생성
-const y9 = {};
+var y9 = {};
 // 네임스페이스 객체의 메서드 등록
-y9.type = (data=null) => {
+y9.type = function () {
+    var data = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+
     // data = data || null;
-    return Object.prototype.toString.call(data).toLowerCase().slice(8,-1);
+    return Object.prototype.toString.call(data).toLowerCase().slice(8, -1);
 };
 
 // 조건 검증 코드 실행
@@ -45,7 +54,7 @@ var data = arr;
 // if ( typeof data === 'array' ) {
 
 // 문제를 해결한 커스텀 유틸리티 함수를 사용하는 방식
-if ( y9.type(data) === 'array' ) {
+if (y9.type(data) === 'array') {
     console.log('data is array.');
 } else {
     console.log('data is not array.');
