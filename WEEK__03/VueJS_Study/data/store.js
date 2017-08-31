77e79edcd57c2fdaf479e3a70236488e87342805
Jee Hoon <yamoo9@naver.com>
2017-08-31 14:18:59 +0900

    store.js 랜덤 메시지 출력 기능

--- WEEK__03/VueJS_Study/data/store.js
+++ WEEK__03/VueJS_Study/data/store.js
@@ -2,8 +2,15 @@
 (function(global){
   'use strict';
 
+  var message_list = [
+    'Hello Modern JavaScript',
+    'Hi! Evenryone.',
+    'Oops!! This is BAD Situation.',
+    'It\'s so Beautiful!'
+  ];
+
   global.store = {
-    message: 'Hello Modern JavaScript',
+    message: message_list[Math.floor(Math.random() * message_list.length)],
     current_year: new Date().getFullYear()
   };
 
