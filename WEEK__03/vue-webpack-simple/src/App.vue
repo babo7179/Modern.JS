6bfdc2dac309fe3786af66e516321da7dd0c039f
Jee Hoon <yamoo9@naver.com>
2017-08-31 18:07:00 +0900

    Vue CLI 활용

---
+++ WEEK__03/vue-webpack-simple/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <!-- 정적 데이터 바인딩 -->
+    <!-- <img src="./assets/food-truck.jpg" alt="다시 만난 세계, 푸드 트럭"> -->
+    <!-- 동적 데이터 바인딩 -->
+    <img :src="image.source" :alt="image.alternate_text">
+    <app-header @change-header-content="changeHeaderContent" :message="msg"></app-header>
+  </div>
+</template>
+
+<script>
+  import AppHeader from './components/pages/Header.vue';
+  export default {
+    name: 'app',
+    components: { AppHeader },
+    data () {
+      return {
+        msg: 'Food Truck App',
+        image: {
+          source: require('./assets/food-truck.jpg'),
+          alternate_text: '다시 만난 세계, 푸드 트럭'
+        }
+      }
+    },
+    methods: {
+      changeHeaderContent(payload){
+        this.msg = payload;
+      }
+    }
+  }
+</script>
+
+<style lang="sass">
+  $bg-color: #eee
+  $fg-color: #212121
+  // --------------------
+  body
+    color: $fg-color
+  #app
+    width: 80%
+    max-width: 1200px
+    margin:
+      left: auto
+      right: auto
+    background: $bg-color
+    text-align: center
+
+</style>
---
+++ WEEK__03/vue-webpack-simple/src/components/pages/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>{{ message }}</h1>
+    <button type="button" @click="changeHeading">change heading</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+    changeHeading(){
+      this.$emit('change-header-content', '다시 만난 우리들의 환상 세계');
+    }
+  }
+}
+</script>
+
